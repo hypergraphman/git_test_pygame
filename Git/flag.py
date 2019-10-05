607421e876dcda4e98451e61c4704baac075b677
ruslanaitov <hypergraphman@gmail.com>
2019-10-05 09:43:54 +0300

    разметка цветов флага

--- Git/flag.py
+++ Git/flag.py
@@ -6,16 +6,20 @@ size = width, height = 300, 300
 screen = pygame.display.set_mode(size)
 
 
-def draw_rect(point_start, point_finish, color):
+def draw_rect(point_start, width_height, color, width_border):
     rect_color = pygame.Color(color)
-    rect_width = 0
-    rect_point = [point_start, point_finish]
+    rect_width = width_border
+    rect_point = [point_start, width_height]
 
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
 
 
-draw_rect((0, 0), (300, 300), 'White')
-draw_rect((10, 10), (10, 280), 'Black')
+draw_rect((0, 0), (300, 300), 'White', 0)
+draw_rect((10, 10), (10, 280), 'Black', 0)
+draw_rect((19, 20), (270, 51), 'Black', 1)
+draw_rect((19, 70), (270, 51), 'Black', 1)
+draw_rect((19, 120), (270, 51), 'Black', 1)
+
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
